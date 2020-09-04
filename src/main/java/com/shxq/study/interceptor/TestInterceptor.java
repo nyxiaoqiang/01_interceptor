b62290492303f694dbbc5e1db8bd3402872b54dd
李雪强 <cx_lixq@123.com>
2020-09-04 16:54:22 +0800

    保存

--- src/main/java/com/shxq/study/interceptor/TestInterceptor.java
+++ src/main/java/com/shxq/study/interceptor/TestInterceptor.java
@@ -7,15 +7,33 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 @Component
 public class TestInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        //获取body里的请求参数
+        BufferedReader streamReader = new BufferedReader( new InputStreamReader(request.getInputStream(), "UTF-8"));
+        StringBuilder responseStrBuilder = new StringBuilder();
+        String inputStr;
+        while ((inputStr = streamReader.readLine()) != null)
+            responseStrBuilder.append(inputStr);
+
+
+
+        JSONObject jsonObject1 = JSONObject.parseObject(responseStrBuilder.toString());
+        System.out.println(jsonObject1);
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("name","haha");
         jsonObject.put("age","12");
+
+        //返回中文会乱码,加上这个就好了
+        response.setCharacterEncoding("utf-8");
+        //返回Json格式的数据
         PrintWriter writer = response.getWriter();
         writer.append(jsonObject.toJSONString());
         return false;
